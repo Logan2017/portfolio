69909af7d30de58eb8595495b4af5fa9e7e9e11f
Juan López <91750051+jlataciertocom@users.noreply.github.com>
2025-04-14 16:02:16 +0200

    flecha arriba

---
+++ js/js.js
@@ -0,0 +1,12 @@
+const elementos = document.querySelectorAll('a.up');
+const puntoAparicion = 2400;
+
+window.addEventListener('scroll', () => {
+  elementos.forEach(el => {
+    if (window.scrollY > puntoAparicion) {
+      el.classList.add('visible');
+    } else {
+      el.classList.remove('visible');
+    }
+  });
+});
