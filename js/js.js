442406706d419330cadb9d79ffc2d8401cb9b503
Juan López <91750051+jlataciertocom@users.noreply.github.com>
2025-04-15 16:35:12 +0200

    mejoras menu mobile

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+// Función para aparición de la flecha de ir hacia arriba
+
 const elementos = document.querySelectorAll('.upWrapper');
 const puntoAparicion = 2400;
 
@@ -10,3 +12,25 @@ window.addEventListener('scroll', () => {
     }
   }); 
 });
+
+
+// Función de hacer blur en el main en menú mobile
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileIcon = document.querySelector('.mobile-icon');
+    const main = document.querySelector('main');
+  
+    if (mobileIcon && main) {
+      const toggleBlur = () => {
+        if (mobileIcon.checked) {
+          main.style.filter = 'blur(10px)';
+        } else {
+          main.style.filter = 'none';
+        }
+      };
+  
+      mobileIcon.addEventListener('change', toggleBlur);
+      toggleBlur(); // Por si ya está marcado al cargar
+    }
+  });
+  
